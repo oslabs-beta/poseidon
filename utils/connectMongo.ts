980875aa104e5a399e159cdf61cea54bada045df
JoshDPT <jjewcu@gmail.com>
2023-01-19 22:28:52 -0500

    create route handler switch - test

--- utils/connectMongo.ts
+++ utils/connectMongo.ts
@@ -3,8 +3,6 @@ import getConfig from 'next/config'
 
 mongoose.set('strictQuery', false);
 
-// const URI = "mongodb+srv://xtchow:poseidon@cluster0.lkfnb5l.mongodb.net/?retryWrites=true&w=majority"
-
 const { serverRuntimeConfig } = getConfig()
 
 const connectMongo = async () => mongoose.connect(serverRuntimeConfig.MONGO_URI);
