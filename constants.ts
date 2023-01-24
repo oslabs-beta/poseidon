3bb595e3f969cc05fab21b93731c370549809a93
wmoody6293 <wmoody6293@gmail.com>
2023-01-24 16:57:50 -0500

    updated grafana iframes
    Co-authored-by: alyagraf <alyagraf@users.noreply.github.com>
    Co-authored-by: Josh Edwards <JoshDPT@users.noreply.github.com>
    Co-authored-by: XTCHOW <xtchow@users.noreply.github.com>

---
+++ constants.ts
@@ -0,0 +1,11 @@
+export const name: string = 'diB0CbTVz';
+export const dashUrls: string[] = [
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674591611424&to=1674595211424&panelId=2`,
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674591663279&to=1674595263279&panelId=3`,
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674591700520&to=1674595300520&panelId=4`,
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674591741986&to=1674595341986&panelId=5`,
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674591795402&to=1674595395402&panelId=6`,
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674593355282&to=1674596955282&theme=dark&panelId=7`,
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674593445278&to=1674597045278&panelId=8`,
+  `http://localhost:3300/d-solo/${name}/node-exporter-nodes?orgId=1&refresh=30s&from=1674593500146&to=1674597100146&panelId=9`,
+];
