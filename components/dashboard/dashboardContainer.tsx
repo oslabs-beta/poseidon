42bcc6d5308ff42084dd89329dad5c07baeccbd4
JoshDPT <jjewcu@gmail.com>
2023-01-25 18:07:19 -0500

    add styling
    Co-authored-by: Will Moody <wmoody6293@users.noreply.github.com>

--- components/dashboard/dashboardContainer.tsx
+++ components/dashboard/dashboardContainer.tsx
@@ -3,24 +3,25 @@ import Grafana from './grafana';
 import { dashUrls } from '../../constants';
 
 export default function DashboardContainer() {
-  const visualizers = dashUrls.map((url, i) => <Grafana key={i} url={url} />);
+	const visualizers = dashUrls.map((url, i) => <Grafana key={i} url={url} />);
 
-  // How to retrieve the unique identifier for dashboard -> https://grafana.com/docs/grafana/latest/developers/http_api/dashboard/
-  // GET /api/dashboards/uid/:uid
-  // Will return the dashboard given the dashboard unique identifier (uid).
-  // Information about the unique identifier of a folder containing the requested dashboard might be found in the metadata.
+	// How to retrieve the unique identifier for dashboard -> https://grafana.com/docs/grafana/latest/developers/http_api/dashboard/
+	// GET /api/dashboards/uid/:uid
+	// Will return the dashboard given the dashboard unique identifier (uid).
+	// Information about the unique identifier of a folder containing the requested dashboard might be found in the metadata.
 
-  // Using d3 with grafana -> https://grafana.com/tutorials/build-a-panel-plugin-with-d3/
+	// Using d3 with grafana -> https://grafana.com/tutorials/build-a-panel-plugin-with-d3/
 
-  return (
-    <div>
-      <section className="bg-slate-900 flex flex-wrap text-gray-600 body-font w-full shadow-inner">
-        <div className="container flex flex-wrap px-5 mx-auto">
-          <div className="container flex justify-center flex-wrap m-4 w-fit">
-            {visualizers}
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div>
+			<section className="bg-slate-900 flex flex-wrap text-gray-600 body-font w-full shadow-inner">
+				<div className="container flex flex-wrap justify-between mx-auto w-30 px-12 py-5">
+					{/* justify-around */}
+					{/* <div className="container flex justify-center flex-wrap m-4 w-fit"> */}
+					{visualizers}
+				</div>
+				{/* </div> */}
+			</section>
+		</div>
+	);
 }
--- components/dashboard/grafana.tsx
+++ components/dashboard/grafana.tsx
@@ -1,10 +1,18 @@
-export default function Grafana(props:any) {
-  const { url } = props;
-  return (
-    <div>
-      <div className="bg-slate-800 border-slate-700 p-6 m-2 rounded-lg w-fit shadow-lg shadow-slate-700/50 hover:scale-105 transition duration-300 ease-in-out ">
-        <iframe src={url} className='grow-0' width="650" height="200" frameBorder="0" ></iframe>
-      </div>
-    </div>
-  );
+export default function Grafana(props: any) {
+	const { url } = props;
+	return (
+		<div className="flex flex-wrap z-10 bg-slate-800 shadow-neutral-900 shadow-2xl p-2 my-5 mx-auto rounded-xl dark:ring-1 dark:ring-inset dark:ring-white/10 hover:scale-105 transition duration-300 ease-in-out">
+			{/* p-2 mx-3 my-3 */}
+			{/* <div className="bg-slate-800 border-slate-700 p-6 m-2 rounded-lg w-fit shadow-lg shadow-slate-700/50  "> */}
+
+			{/* relative z-10 -ml-10 col-span-3 bg-slate-800 rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10 p-5' */}
+			<iframe
+				src={url}
+				className="grow-0"
+				width="650"
+				height="200"
+				frameBorder="0"
+			></iframe>
+		</div>
+	);
 }
