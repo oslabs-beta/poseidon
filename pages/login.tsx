9613c99e9e96072b6df625f159731f73ba69e32b
JoshDPT <jjewcu@gmail.com>
2023-01-25 21:08:50 -0500

    add styling

--- pages/login.tsx
+++ pages/login.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
 
       {/* <div className="bg-no-repeat bg-left-top" style={"background-image: url('../public/backgroundImg.png');"}></div> */}
       
-        <main className='relative z-10 -ml-10 col-span-3 bg-slate-800 rounded-xl xl:ml-0 dark:ring-1 dark:ring-inset dark:ring-white/10 p-5  shadow-neutral-900 shadow-2xl'>
+        <main className='relative z-10 -ml-10 col-span-3 bg-slate-800 rounded-xl xl:ml-0 dark:ring-1 dark:ring-inset dark:ring-white/10 p-5  shadow-neutral-900 shadow-2xl space-y-2'>
           
           <div>
             <LoginForm/>
--- pages/register.tsx
+++ pages/register.tsx
@@ -5,17 +5,20 @@ import RegisterForm from '../components/register';
 export default function Register() {
   return (
     <div className='grid h-screen place-items-center bg-slate-900'>
+
       <Head>
         <title>Sign Up Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='bg-slate-800 container mx-auto w-80 grid-cols-1 gap-0 content-center rounded '>
+
+      <main className='relative z-10 -ml-10 col-span-3 bg-slate-800 rounded-xl xl:ml-0 dark:ring-1 dark:ring-inset dark:ring-white/10 p-5  shadow-neutral-900 shadow-2xl space-y-2'>
+
         <div>
           <RegisterForm/>
         </div>
         
-        <div className='flex items-center justify-center'>
+        <div className='text-gray-200 flex items-center justify-center'>
           <Link 
             href="/"
           >Back to home</Link> 
