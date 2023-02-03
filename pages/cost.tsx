78c0bfab88f0c3777da816d2eae1bcbe6b9b5a67
wmoody6293 <wmoody6293@gmail.com>
2023-02-02 21:08:30 -0500

    finished cost component logic

--- pages/cost.tsx
+++ pages/cost.tsx
@@ -1,19 +1,85 @@
 import Head from 'next/head';
 import Link from 'next/link';
 // import RegisterForm from '../components/register';
-import CostComponent from '../components/dashboard/costComponent'
+import CostComponent from '../components/dashboard/costComponent';
 import NavBar from '../components/navbar';
 
-export default function Register() {
-  return (
-    <div className=''>
+export default function Cost({ data }: any) {
+  // console.log('This is data within Cost page', data);
 
+  return (
+    <div className="">
       <Head>
         <NavBar />
         <title>Cost Analysis</title>
-        <CostComponent />
+        <CostComponent data={data} />
       </Head>
-
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  //in terminal, type kubectl get service -n kubecost and put external ip in kubecostIP value here:
+  // const kubecostIP = '127.0.0.1';
+  const URL = `http://127.0.0.1:9090/model/allocation?window=15d&aggregate=cluster`;
+  const response = await fetch(URL);
+  const parsed = await response.json();
+
+  console.log('parsed data inside getServerSideProps: ', parsed.data);
+
+  if (parsed) {
+    const parsedArr = parsed.data;
+    function getProps(parsedArr: []) {
+      let totalCPU = 0;
+      let totalRAM = 0;
+      let totalGPU = 0;
+      let totalloadBalancerCost = 0;
+      let totalNetworkCost = 0;
+      let totalPVCost = 0;
+      let totalCost = 0;
+      let totalSharedCost = 0;
+      let totalDays = 0;
+      // add cost efficiency
+
+      function getMetrics(metricObject: any) {
+        for (const key in metricObject) {
+          if (key === 'cpuCost') totalCPU += metricObject[key];
+          if (key === 'gpuCost') totalGPU += metricObject[key];
+          if (key === 'networkCost') totalNetworkCost += metricObject[key];
+          if (key === 'loadBalanceerCost')
+            totalloadBalancerCost += metricObject[key];
+          if (key === 'pvCost') totalPVCost += metricObject[key];
+          if (key === 'totalCost') totalCost += metricObject[key];
+          if (key === 'sharedCost') totalSharedCost += metricObject[key];
+        }
+      }
+      parsedArr.forEach((obj: any) => {
+        if (obj.__idle__) {
+          totalDays += 1;
+          for (const key in obj) {
+            getMetrics(obj[key]);
+          }
+        }
+      });
+      const data = {
+        totalCPU,
+        totalRAM,
+        totalGPU,
+        totalloadBalancerCost,
+        totalNetworkCost,
+        totalPVCost,
+        totalCost,
+        totalSharedCost,
+        totalDays,
+      };
+      console.log('This is data after all our friggin work: ', data);
+
+      // NodeHourlyCost = NORMALIZED_GPU_PRICE * # of GPUs + NORMALIZED_CPU_PRICE * # of CPUs + NORMALIZED_RAM_PRICE * # of RAM GB
+
+      return { props: { data } };
+    }
+    return getProps(parsedArr);
+  } else {
+    console.log('error! no data retrieved from kubecost!!');
+  }
 }
