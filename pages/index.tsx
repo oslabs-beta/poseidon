adcecf5a8d5d2bff6689143df12496d2338edc5c
JoshDPT <jjewcu@gmail.com>
2023-02-04 13:42:38 -0500

    add dashboard / cost redirect to login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import DashboardContainer from '../components/dashboard/dashboardContainer';
 import NavBar from '../components/navbar';
 import Footer from '../components/footer';
@@ -16,49 +14,56 @@ import { getServerSession } from 'next-auth/next';
 
 // // export default function Layout({ props: Props }: ReactElement) {
 
+// THIS IS THE CODE THAT WORKED BEFORE SERVER AUTH IMPLEMENTATION
+// export default function Home() {
+//   const { data: session } = useSession();
+//   const dashboard = session ? <DashboardContainer /> : null;
+
+
+//   return (
+//     <div>
+//       <Head>
+//         <title>Poseidon</title>
+//       </Head>
+//       <NavBar />
+//       {dashboard}
+//       <Footer />
+//     </div>
+//   )
+// }
+
+
 export default function Home() {
-  const { data: session } = useSession();
-  const dashboard = session ? <DashboardContainer /> : null;
-  // if (!session) {
-  //   setTimeout(() => {
-  //     signIn();
-  //   }, '1000');
-  // }
+  // const { data: session } = useSession();
+  // const dashboard = session ? <DashboardContainer /> : null;
+  // if(!session) signIn();
+
   return (
-    <>
-      <div>
-        <Head>
-          <title>Poseidon</title>
-        </Head>
-        <NavBar />
-        {dashboard}
-        <Footer />
-      </div>
-    </>
-  );
+    <div>
+      <Head>
+        <title>Poseidon</title>
+      </Head>
+      <NavBar />
+      <DashboardContainer /> 
+      <Footer />
+    </div>
+  )
 }
 
-// export async function getServerSideProps(context: any) {
-//   const session = await getServerSession(context.req, context.res, authOptions);
+// this is a server side retreival of the session cookie, which then returns the props to the component above- allows the page to not even render if no cookie
+export async function getServerSideProps(context: any) {
+  const session = await getServerSession(context.req, context.res, authOptions);
 
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: '/',
-//         permanent: false,
-//       },
-//     };
-//   }
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/signin',
+        permanent: false,
+      },
+    }
+  }
 
-//   return {
-//     props: {
-//       session,
-//     },
-//   };
-// }
-
-// export function getServerSideProps() {
-//   return {
-//     props: { message: 'Welcome to the About Page' },
-//   };
-// }
+  return {
+    props: { session }
+  }
+}
