a254f577e36c08f9075f8fb64b20817e7e118f66
JoshDPT <jjewcu@gmail.com>
2023-02-06 13:26:03 -0500

    add svg to login btn - commented out for now

--- components/login-btn.tsx
+++ components/login-btn.tsx
@@ -15,7 +15,11 @@ export default function LoginButton() {
           onClick={
             () => signOut()
           }
-        >Sign out
+        >
+          {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+          </svg> */}
+          Sign out
         </button>
       </>
     )
@@ -27,7 +31,12 @@ export default function LoginButton() {
         onClick={
           () => signIn()
         }
-      >Sign in</button>
+      >
+        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+        </svg> */}
+        Sign in
+      </button>
     </>
   )
 }
